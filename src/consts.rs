9c3d67eaaea0327c2e64b1ec583a3d700935dda2
DaXcess <daxcess@daxcess.io>
2023-06-08 16:26:07 +0200

    Release moment

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,6 @@
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-pub const MOTD: &str = "PRE-RELEASE (v2)";
+pub const MOTD: &str = "some good 'ol music";
 
 /// The time it takes for Spoticord to disconnect when no music is being played
 pub const DISCONNECT_TIME: u64 = 5 * 60;
 
-// pub const MOTD: &str = "some good 'ol music";
